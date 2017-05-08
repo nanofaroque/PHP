eadf55bc98a48447bc2aa5137024b1a8ad061670
Omar Faroque <omar.faroque@realpage.com>
2017-05-08 16:51:29 -0500

    initial commit and adding constants file

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+define ('STRINT_CONSTANT_FIRST',"first");
+define ('STRINT_CONSTANT_SECOND',"second");
+define ('STRINT_CONSTANT_THIRD',"third");
+define ('MY_INT_FIRST',5);
+define ('MY_INT_SECOND',3);
+?>
